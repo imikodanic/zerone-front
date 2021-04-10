faee8f7f2ba3abdf691d21b71ed247248452b095
Ivan Mikodanic <ivanmikodanic9@gmail.com>
2021-04-10 14:59:26 +0200

    feat(Pages): groups page title

---
+++ components/pages/groups/GroupPreviewCard.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: 'GroupPreviewCard',
+  props: {
+    group: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+<template>
+  <div></div>
+</template>
--- pages/groups/index.vue
+++ pages/groups/index.vue
@@ -1,5 +1,31 @@
+<script>
+export default {}
+</script>
+
 <template>
-  <div>
-    <h1>Groups</h1>
+  <div class="groups-container">
+    <h1 class="text-4xl font-bold mt-14">Zerone Project Groups</h1>
+    <p class="text-2xl mt-4">
+      Browse through
+      <span class="highlighted-word">5 project groups</span> and explore the
+      ones you're interested in.
+      <br />
+      Our projects are carefully organized so you can get
+      <span class="highlighted-word"> the right information </span> faster.
+    </p>
   </div>
 </template>
+
+<style scoped>
+.groups-container {
+  background-image: url('~/static/pages/projects/bg-dots.svg');
+  @apply bg-no-repeat;
+  @apply bg-contain;
+  @apply bg-center;
+
+  @apply px-5 lg:px-28;
+}
+.highlighted-word {
+  @apply text-primary-purple font-bold;
+}
+</style>
