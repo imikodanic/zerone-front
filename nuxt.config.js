22c2a0ed4374a98441d8546bf2ae1d023e66195a
Ivan Mikodanic <ivanmikodanic9@gmail.com>
2021-05-26 19:41:29 +0200

    fix(Config): remove transitioncss from nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['~/assets/baseStyle.css', '~/assets/transitions.css'],
+  css: ['~/assets/baseStyle.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ['~/plugins/globalComponents.js', '~/plugins/axios.js'],
