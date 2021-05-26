b8bb2c9ca6d8434f165c6528dea3d8f7f3b13b4d
Ivan Mikodanic <ivanmikodanic9@gmail.com>
2021-05-26 20:35:55 +0200

    style(Layout): default layout page minimum height

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,8 +14,26 @@ export default {
   <div class="default-layout">
     <navbar />
     <div class="pt-20 lg:pt-24">
-      <Nuxt />
+      <Nuxt class="min-h-page" />
       <default-footer />
     </div>
   </div>
 </template>
+
+<style scoped>
+.min-h-page {
+  min-height: calc(100vh - 344px - 96px);
+}
+
+@media screen and (max-width: 1024px) {
+  .min-h-page {
+    min-height: calc(100vh - 328px - 96px);
+  }
+}
+
+@media screen and (max-width: 640px) {
+  .min-h-page {
+    min-height: calc(100vh - 464px - 80px);
+  }
+}
+</style>
