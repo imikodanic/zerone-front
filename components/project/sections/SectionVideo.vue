e42680a1f548f4d55ef01193d540a5800d7e26e1
Ivan Mikodanic <ivan.mikodanic@gauss.hr>
2022-01-24 22:17:16 +0100

    feat(Components): section video format youtube url

--- components/project/sections/SectionVideo.vue
+++ components/project/sections/SectionVideo.vue
@@ -11,6 +11,22 @@ export default {
       default: false,
     },
   },
+  computed: {
+    formattedURL() {
+      if (!this.value) return null
+
+      const queryString = this.value.split('?')[1]
+
+      if (!queryString) return
+
+      const urlSearchParams = new URLSearchParams(queryString)
+      const params = Object.fromEntries(urlSearchParams.entries())
+
+      const baseURL = 'https://www.youtube.com/embed/'
+
+      return baseURL + params.v
+    },
+  },
   methods: {
     setValue(value) {
       this.$emit('input', value)
@@ -21,7 +37,23 @@ export default {
 
 <template>
   <div class="flex justify-center">
-    <t-input v-if="edit" id="video-input" :value="value" @input="setValue" />
-    <iframe v-else :src="value" width="960" height="540"></iframe>
+    <t-input
+      v-if="edit"
+      id="video-input"
+      :value="value"
+      class="w-52"
+      @input="setValue"
+    />
+    <iframe
+      v-else
+      :src="formattedURL"
+      width="960"
+      height="540"
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      class="max-w-4xl"
+    />
   </div>
 </template>
